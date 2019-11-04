d37258bbf856f2f9ccf6decc188ba444e313a41b
Chengfeng Pan <leo.cf.pan@gmail.com>
2019-11-04 10:10:56 +0000

    API test setup

---
+++ test/functional/api/tracingsTest.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const server = require("../../../bin/www");
+const expect = chai.expect;
+const request = require("supertest");
+const _ = require("lodash");
+
+const Tracing = require("../../../models/tracings");
+
+describe('Tracings',  () => {
+    // TODO
+});
